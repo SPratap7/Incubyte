0f610f2b05657cc8c2a7df95a8980855dbda680d
SPratap7 <surya.linkin@gmail.com>
2024-12-18 23:00:23 +0530

    Fixed add function for different delimiter

--- src/main/java/com/calculator/StringCalculator.java
+++ src/main/java/com/calculator/StringCalculator.java
@@ -23,13 +23,22 @@ public class StringCalculator {
         if(numbers.isBlank()) {
             return 0;
         } else {
-            String[] numbersArray = numbers.split("[,\n]");
-            int sum = 0;
-            for(String number : numbersArray) {
-                int current = Integer.parseInt(number);
-                sum += current;
+            String delimiter = ",";
+            if (numbers.matches("//(.*)\n(.*)")) {
+                delimiter = Character.toString(numbers.charAt(2));
+                numbers = numbers.substring(4);
             }
-            return sum;
+            String[] numbersArray = numbers.split(delimiter + "|\n");
+            return arraySum(numbersArray);
         }
+    }
+
+    int arraySum(String[] numbersArray) {
+        int sum = 0;
+        for(String number : numbersArray) {
+            int current = Integer.parseInt(number);
+            sum += current;
+        }
+        return sum;
     }
 }
