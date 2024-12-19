377a4373f87ee03be26fa6af4dc6ef0b559278ea
SPratap7 <surya.linkin@gmail.com>
2024-12-19 20:25:01 +0530

    Added testcase for Customer Delimiter with variable length

--- src/test/java/com/calculator/StringCalculatorTest.java
+++ src/test/java/com/calculator/StringCalculatorTest.java
@@ -21,36 +21,78 @@ class StringCalculatorTest {
     }
 
     @Test
-    void add() {
+    void ShouldReturnZeroOnEmptyString() {
         stringCalculator.setNumbers("");
         assertEquals(0, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldReturnZeroOnWhiteSpace() {
         stringCalculator.setNumbers(" ");
         assertEquals(0, stringCalculator.add());
         stringCalculator.setNumbers("\n");
         assertEquals(0, stringCalculator.add());
         stringCalculator.setNumbers("  \n");
+    }
+
+    @Test
+    void ShouldReturnNumbersOnNumbers() {
         assertEquals(0, stringCalculator.add());
         stringCalculator.setNumbers("0");
         assertEquals(0, stringCalculator.add());
         stringCalculator.setNumbers("1");
         assertEquals(1, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldReturnSumOnNumbersDelimitedByComma() {
         stringCalculator.setNumbers("1,2");
         assertEquals(3, stringCalculator.add());
         stringCalculator.setNumbers("5,4");
         assertEquals(9, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldAcceptNewLineAsDelimiter() {
         stringCalculator.setNumbers("5\n4,1");
         assertEquals(10, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldAcceptCustomDelimiter() {
         stringCalculator.setNumbers("//;\n4;1");
         assertEquals(5, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldThrowErrorWithNegativeNumbers() {
         try {
             stringCalculator.setNumbers("-4,1");
             stringCalculator.add();
         } catch (IllegalArgumentException e) {
             assertEquals("Negatives not allowed: -4", e.getMessage());
         }
+    }
+
+    @Test
+    void ShouldNotAcceptNumberGreaterThanThousand() {
         stringCalculator.setNumbers("4,1001");
         assertEquals(4, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldAcceptCustomDelimiterOfAnyLength(){
         stringCalculator.setNumbers("//[***]\n1***2***3");
+        assertEquals(6, stringCalculator.add());
+    }
+
+    @Test
+    void ShouldAcceptMultipleCustomDelimiterOfAnyLength(){
+        stringCalculator.setNumbers("//[*][%]\n1*2%3");
+        assertEquals(6, stringCalculator.add());
+        stringCalculator.setNumbers("//[**][%]\n1**2%3");
+        assertEquals(6, stringCalculator.add());
+        stringCalculator.setNumbers("//[*][%%]\n1*2%%3");
         assertEquals(6, stringCalculator.add());
     }
 
